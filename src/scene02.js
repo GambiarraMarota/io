07ce38c146c12a5e31132111092f08a0a078271f
João Vitor Rebouças <jvitor@dcc.ufrj.br>
2016-06-25 19:25:15 -0300

    caixa 'pontilhada' criada

--- src/scene02.js
+++ src/scene02.js
@@ -1,37 +1,47 @@
 var scene02 = new Scene();
 
+var textX;
+var textY;
 var dottedBox;
+var clicked;
 
 scene02.create = function() {
-    createCartesianPlan();
-    createCoordText();
-    drawDottedBox(false);
-
+    this.firstPass = true;
+    clicked = false;
+    scene01.finished = true;
     this.tap = false;
     game.input.onTap.add(this.onTap,this);
 }
 
 scene02.render = function() {
-
+    graphics.clear();
+    this.createCoordText();
+    createInnerCanvas();
+    createCartesianPlan();
+    this.drawDottedBox(clicked);
 }
 
 scene02.update = function() {
-    
     if (this.tap) {
         this.tap = false;
     }
+    console.log("teste");
 }
 
 //TODO: mudar numero mágico
-function createCoordText(){
-    var textX = game.add.text(CANVAS_WIDTH-15, CANVAS_HEIGHT/2, "X", { font: "15px Arial", fill: "#000000", align: "center" });
-    var textY = game.add.text(CANVAS_WIDTH/2+5, CANVAS_ORIGIN_Y, "Y", { font: "15px Arial", fill: "#000000", align: "center" });
+scene02.createCoordText = function(){
+    if (this.firstPass){
+        this.textX = game.add.text(CANVAS_WIDTH-15, CANVAS_HEIGHT/2, "X", { font: "15px Arial", fill: "#000000", align: "center" });
+        this.textY = game.add.text(CANVAS_WIDTH/2+5, CANVAS_ORIGIN_Y, "Y", { font: "15px Arial", fill: "#000000", align: "center" });
+        this.firstPass = false;
+    }
 }
 
-function drawDottedBox(clicked){
+scene02.drawDottedBox = function(clicked){
     var margin = 30
     if (!clicked){
         graphics.lineStyle(1, 0xFF0000, 1);
+        console.logsdsfs
     } else {
         graphics.lineStyle(1, 0xFFFF00, 1);
     }
@@ -40,6 +50,12 @@ function drawDottedBox(clicked){
 
 scene02.onTap = function(){
     this.tap = true;
-    console.log("Teste");
-    drawDottedBox(true);
+    this.prepareToDrag();
+}
+
+scene02.prepareToDrag = function(){
+    var a = { font: "15px Arial", fill: '#FFFF00', align: "center" };
+    clicked = true;
+    this.textX.setStyle(a);
+    this.textY.setStyle(a);
 }
