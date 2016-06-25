db2a60cae2effab9e2177f26bd82f982270c9acf
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2016-06-25 12:04:41 -0300

    adding files

---
+++ src/main.js
@@ -0,0 +1,17 @@
+var game = new Phaser.Game(WINDOW_WIDTH, WINDOW_HEIGHT, Phaser.AUTO, 'main-canvas', { preload: preload, create: create, render: render, update: update });
+
+function preload(){
+
+}
+
+function create() {
+    game.backgroundColor = '#cccccc';
+}
+
+function render() {
+    // body...
+}
+
+function update() {
+    // body...
+}
