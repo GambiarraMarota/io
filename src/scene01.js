40bbde048e8f2a40216020cae5dcc76e1ddd978c
RafaelGiordanno <rafael_giordanno@hotmail.com>
2016-06-26 10:56:55 -0300

    scene01 com tweening

--- src/scene01.js
+++ src/scene01.js
@@ -8,10 +8,14 @@ scene01.create = function() {
     // declarações de pontos objetivos
     this.targetDotX = new Phaser.Circle(CANVAS_WIDTH - 96 , CANVAS_HEIGHT / 2, 48);
     this.targetDotX.color = 0xff9f00;
+    this.targetDotX.stateTime = .15;
+    
     this.targetDotY = new Phaser.Circle(CANVAS_WIDTH / 2, 64, 48);
     this.targetDotY.color = 0xcc6633;
+    this.targetDotY.stateTime = 0;
 
     this.touchingScreen = false;
+    this.stateTime = 0;
     
     // Estados de tela para essa cena
     this.currentState = 0;
@@ -32,12 +36,24 @@ scene01.update = function() {
                 this.dot.x = CANVAS_WIDTH / 2;
                 this.dot.y = CANVAS_HEIGHT / 2;
                 this.currentState++;
+                // game.time.events.add(.2, this.shrinkTargetX, this);
             }
         } else if (this.currentState == 1) { // segundo alvo: eixo y
+            // disappear with target x smoothly
+            // this.targetDotX.diameter = Phaser.Math.linear(this.targetDotX.diameter, 0, .2);
+            // t: current time, b: begInnIng value, c: change In value, d: duration
+            this.targetDotX.diameter = easeInBack(this.targetDotX.stateTime, 50 , -1, .2);
             if (Phaser.Circle.intersects(this.dot, this.targetDotY)) {
                 this.dot.x = CANVAS_WIDTH / 2;
                 this.dot.y = CANVAS_HEIGHT / 2;
                 this.currentState++;
+            }
+            console.log(this.targetDotX.stateTime);
+            this.targetDotX.stateTime += game.time.elapsed/1000.0;
+        } else {
+            this.targetDotY.diameter = Phaser.Math.linear(this.targetDotY.diameter, 0, .2);
+            if (this.targetDotY.diameter < 3) {
+                this.finished = true;
             }
         }
     }
@@ -48,6 +64,10 @@ scene01.update = function() {
             this.dot.y = game.input.position.y;
         }
     }
+}
+
+scene01.shrinkTargetX = function() {
+    this.targetDotX.diameter -= 4;
 }
 
 scene01.render = function() {
@@ -62,25 +82,51 @@ scene01.render = function() {
         this.renderHorizontalTrail();
         this.renderVerticalPreTrail();
     }else {
-        this.finished = true;
         createCartesianPlan();
     }
     
     // círculos alvo
-    graphics.lineStyle(5, this.currentState == 0 ? 0x99aabb : 0xcccccc);
-    graphics.drawCircle(this.targetDotX.x, this.targetDotX.y, this.targetDotX.diameter);
-    
-    graphics.lineStyle(5, this.currentState <= 1 ? 0x99aabb : 0xcccccc);
-    graphics.drawCircle(this.targetDotY.x, this.targetDotY.y, this.targetDotY.diameter);
+    if (this.currentState >= 0) { // círculo do eixo x
+        graphics.lineStyle(5, this.currentState == 0 ? 0x99aabb : 0xff9f00);
+        graphics.drawCircle(this.targetDotX.x, this.targetDotX.y, this.targetDotX.diameter
+                            + Phaser.Math.clamp(Math.sin(game.time.totalElapsedSeconds() * 2), -1, 1) * 4);
+        
+        if (this.currentState > 0) {
+            graphics.lineStyle(5, 0xff9f00);
+            var clampedDistance = Phaser.Math.clamp(Math.sin(game.time.totalElapsedSeconds() * 6), 0, 1) * 6;
+            graphics.moveTo(CANVAS_WIDTH - 112 + clampedDistance, CANVAS_CENTER_Y - 12, this.targetDotX.diameter);
+            graphics.lineTo(CANVAS_WIDTH - 94 + clampedDistance, CANVAS_CENTER_Y, this.targetDotX.diameter);
+            graphics.lineTo(CANVAS_WIDTH - 112 + clampedDistance, CANVAS_CENTER_Y + 12, this.targetDotX.diameter);
+        }
+        
+    }
+
+    if (this.currentState > 0) {
+        console.log("hahahahah");
+        graphics.lineStyle(5, this.currentState < 2 ? 0x99aabb : 0xcc6633);
+        graphics.drawCircle(this.targetDotY.x, this.targetDotY.y, this.targetDotY.diameter
+                            + Phaser.Math.clamp(Math.sin(game.time.totalElapsedSeconds() * 2), -1, 1) * 4);
+        
+        if (this.currentState > 1) {
+            graphics.lineStyle(5, 0xcc6633);
+            var clampedDistance = Phaser.Math.clamp(Math.sin(game.time.totalElapsedSeconds() * 6), 0, 1) * 6;
+            graphics.moveTo(CANVAS_CENTER_X - 12, 80 - clampedDistance, this.targetDotX.diameter);
+            graphics.lineTo(CANVAS_CENTER_X, 64 - clampedDistance, this.targetDotX.diameter);
+            graphics.lineTo(CANVAS_CENTER_X + 12, 80 - clampedDistance, this.targetDotX.diameter);
+        }
+    } 
     
     // renderiza o círculo que o usuário pode controlar
     graphics.lineStyle(5, 0x29abe2);
-    graphics.drawCircle(this.dot.x, this.dot.y, this.dot.diameter);
+    graphics.drawCircle(this.dot.x, this.dot.y,
+                        this.dot.diameter + Phaser.Math.clamp(Math.sin(game.time.totalElapsedSeconds() * 4) + .6, 0, 1) * 5);
     graphics.lineStyle(20,0x29abe2);
-    graphics.drawCircle(this.dot.x, this.dot.y, this.dot.diameter/5);
+    graphics.drawCircle(this.dot.x, this.dot.y,
+                        this.dot.diameter/5 + Phaser.Math.clamp(Math.sin(game.time.totalElapsedSeconds() * 4), 0, 1) * 5);
 }
+
 scene01.renderHorizontalPreTrail = function(){
-    graphics.lineStyle(4,0x444444);
+    graphics.lineStyle(4,0x444444, .4);
     graphics.moveTo(CANVAS_CENTER_X, CANVAS_CENTER_Y);
     graphics.lineTo(CANVAS_WIDTH - 96 , CANVAS_CENTER_Y);
     //traços verticais pra artificiar o pontilhado,tentei usando for mas nao funcionou #HARDCODE!
@@ -93,12 +139,14 @@ scene01.renderHorizontalPreTrail = function(){
     graphics.lineTo(CANVAS_CENTER_X + 180,CANVAS_CENTER_Y + 50);
     
 }
+
 scene01.renderHorizontalTrail = function() {
     graphics.lineStyle(4, 0xff9f00);
     graphics.moveTo(CANVAS_CENTER_X, CANVAS_CENTER_Y);
     graphics.lineTo(CANVAS_WIDTH - 96, CANVAS_CENTER_Y);
         
 }
+
 scene01.renderVerticalPreTrail = function(){
     graphics.lineStyle(4,0x444444); 
     graphics.moveTo(CANVAS_CENTER_X,CANVAS_CENTER_Y);
