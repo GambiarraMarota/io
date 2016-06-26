2a5beb05d04943a82ab7fce9ed9c32d9ec1e8bae
João Vitor Rebouças <jvitor@dcc.ufrj.br>
2016-06-25 21:41:10 -0300

    done, wtf

--- src/scene02.js
+++ src/scene02.js
@@ -4,6 +4,7 @@ var textX;
 var textY;
 var dottedBox;
 var clicked;
+var ready = 0;
 
 scene02.create = function() {
     this.firstPass = true;
@@ -41,11 +42,13 @@ scene02.drawDottedBox = function(clicked){
     var margin = 30
     if (!clicked){
         graphics.lineStyle(1, 0xFF0000, 1);
-        console.logsdsfs
+    } else if (clicked && (ready >= 2)){
+        graphics.lineStyle(1, 0x00FF00, 1);
     } else {
         graphics.lineStyle(1, 0xFFFF00, 1);
     }
     dottedBox = graphics.drawRect(CANVAS_WIDTH + margin,CANVAS_ORIGIN_Y + margin, 100, CANVAS_HEIGHT/3 - margin);
+    retangulo = new Phaser.Rectangle(CANVAS_WIDTH + margin,CANVAS_ORIGIN_Y + margin, 100, CANVAS_HEIGHT/3 - margin);
 }
 
 scene02.onTap = function(){
@@ -54,8 +57,42 @@ scene02.onTap = function(){
 }
 
 scene02.prepareToDrag = function(){
-    var a = { font: "15px Arial", fill: '#FFFF00', align: "center" };
     clicked = true;
+
+    //muda a cor da fonte para ajudar a ver o drag and drop
+    var a = { font: "15px Arial", fill: '#FFFF00', align: "center" };
     this.textX.setStyle(a);
     this.textY.setStyle(a);
+
+    //habilita o drag and drop
+    this.dragText(this.textX);
+    this.dragText(this.textY);
+}
+
+scene02.dragText = function(text){
+    text.inputEnabled = true;
+    text.input.enableDrag();
+
+    //text.events.onInputOver.add(over, this);
+    text.events.onInputOut.add(this.out, this);
+
+    //text.events.onInputDown.add(down, this);
+   // text.events.onInputUp.add(this.up, this);
+}
+
+scene02.out = function(item){
+    var b = { font: "15px Arial", fill: '#00FF00', align: "center" };
+    if (retangulo.contains(item.x,item.y)){
+        if(item == this.textX){
+            this.textX.setStyle(b);
+            this.textX.input.disableDrag();
+            this.textX2 = game.add.text(CANVAS_WIDTH-15, CANVAS_HEIGHT/2, "X", { font: "15px Arial", fill: "#000000", align: "center" });
+            ready++;
+        } else {
+            this.textY.setStyle(b);
+            this.textY.input.disableDrag();
+            this.textY2 = game.add.text(CANVAS_WIDTH/2+5, CANVAS_ORIGIN_Y, "Y", { font: "15px Arial", fill: "#000000", align: "center" });
+            ready++;
+        }
+    }
 }
